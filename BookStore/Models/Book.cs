d1c9ade2a3766b711c8b33cc13a771e868b869d9
benjam-rich <70960034+benjam-rich@users.noreply.github.com>
2021-02-13 12:18:42 -0700

    Add project files.

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN { get; set; }
+        public string ClassCat { get; set; }
+        public string Price { get; set; }
+    }
+
+}
---
+++ BookStore/Models/EFBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class EFBookRepository : IBookRepository
+    {
+        private BookDBContext _context;
+        //Constructor
+        public EFBookRepository (BookDBContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
